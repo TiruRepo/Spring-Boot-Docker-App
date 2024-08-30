625323799931189a6a7d30b91ed1e9ab51ef9eae
TiruRepo <111552420+TiruRepo@users.noreply.github.com>
2024-08-30 16:28:42 +0530

    Update SbRestAppApplication.java

--- src/main/java/in/ashokit/SbRestAppApplication.java
+++ src/main/java/in/ashokit/SbRestAppApplication.java
@@ -9,12 +9,9 @@ public class SbRestAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SbRestAppApplication.class, args);
 	}
-<<<<<<< HEAD
+
 	public void doProcess(){
 	
 	}
-=======
-	
 
->>>>>>> 640ce1bde08479fd254e5e12c167f5a3bbe3fb7c
 }
